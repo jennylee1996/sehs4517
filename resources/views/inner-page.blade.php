0512fe5c3cfe76355d85de192191098f507b2b23
Jenny <lee19960807@gmail.com>
2023-11-14 00:26:43 +0800

    Backup

---
+++ resources/views/inner-page.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+@section('content')
+    <!-- ======= Breadcrumbs ======= -->
+    <section class="breadcrumbs">
+        <div class="container">
+
+            <ol>
+                <li><a href="index.blade.php">Home</a></li>
+                <li>Inner Page</li>
+            </ol>
+            <h2>Inner Page</h2>
+
+        </div>
+    </section><!-- End Breadcrumbs -->
+
+    <section class="inner-page">
+        <div class="container" data-aos="fade-up">
+            <p>
+                Example inner page template
+            </p>
+        </div>
+    </section>
+@endsection
